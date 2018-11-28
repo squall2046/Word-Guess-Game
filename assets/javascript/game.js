50a47599066702346e9ce9d53ab073a34e68c338
squall2046 <squall2046@hotmail.com>
2018-11-28 09:58:16 -0500

    correct js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,5 @@
 //Isaac WU Word Guess Game
 
-var gameGuide = document.getElementById("guide-text");
-var guessWord = document.getElementById("guess-word");
-var playerTyped = document.getElementById("player-typed");
-var leftChances = document.getElementById("left-chances");
-var winTimes = document.getElementById("win-text");
-var lossTimes = document.getElementById("losses-text");
-var lossPic = document.getElementById("loss-pic");
-var winPic = document.getElementById("win-pic");
-
-
-
 var hiddenWord = [];
 var tried = [];
 var chances = 9;
@@ -18,69 +7,97 @@ var wins = 0;
 var losses = 0;
 
 var allWords = ["CLOUD", "CAITSITH", "CID", "REDXIII", "SEPHIROTH", "TIFA", "YUFFIE"];
+
+// choose a random word
 var randomNum = Math.floor(Math.random() * allWords.length);
 var randomWord = allWords[randomNum];
+var randomArr = randomWord.split("");
 console.log(randomWord);
+console.log("word length: " + randomWord.length);
+console.log(randomArr);
 
-// hide the random word to _ _ _
+// hide the random word to [ _ _ _ ]
 for (var i = 0; i < randomWord.length; i++) {
-    // hiddenWord.push("_");
-    hiddenWord += ("_ ");
-    //how to make sure the letter is in randomWord ?????????
-    var letter = randomWord[i];
-    //how to instead "_" to right letter ??????????
-    var seeLetter = hiddenWord[i];
-}
-console.log(hiddenWord);
-console.log("letter:" + letter);
-console.log("underscore:" + seeLetter);
+    // hiddenWord += ("_ ");
+    hiddenWord[i] = "_";
+};
+var guessWord = document.getElementById("guess-word");
+guessWord.textContent = "[ " + hiddenWord + " ]";
 
-// click action
+
+/////////// onkeyup action //////////////
 document.onkeyup = function (event) {
     var userGuess = event.key.toUpperCase();
     tried.push(userGuess);
-    lossPic.innerHTML = ' <img src="assets/images/sephiroth.gif" class="leftpic"> ';
-    winPic.innerHTML = ' <img src="assets/images/cloud.gif" class="rightpic"> ';
-    var clickSound = new Audio('assets/sound/01.mp3');
-    clickSound.play();
-    console.log(tried);
-    console.log(userGuess);
+    var lossPic = document.getElementById("loss-pic");
+    lossPic.innerHTML = ' <img src="assets/images/sephiroth.gif" class="leftpic"> '; //reset
+    var winPic = document.getElementById("win-pic");
+    winPic.innerHTML = ' <img src="assets/images/cloud.gif" class="rightpic"> '; //reset
+    var keySound = new Audio('assets/sound/01.mp3');
+    keySound.play();
 
-    // guess right:
-    if (userGuess === letter) {
-        letter = seeLetter;
-    }
     // guess wrong:
-    else if (userGuess !== letter) {
+    if (randomWord.indexOf(userGuess) === -1) {
         chances--;
     }
-    // loss:
     if (chances === 0) {
         losses++;
-        tried = []; //reset
-        chances = 9; //reset
-        randomWord = allWords[randomNum];
+        hiddenWord = [];
+        tried = [];
+        chances = 9;
+        randomWord = allWords[Math.floor(Math.random() * allWords.length)];
+        console.log(randomWord)
+        for (var i = 0; i < randomWord.length; i++) {
+            hiddenWord[i] = "_";
+        };
+        guessWord.textContent = "[ " + hiddenWord + " ]";
+
+
         lossPic.innerHTML = ' <img src="assets/images/sephiroth2.gif" class="leftpic"> ';
         //load the audio location from the html address not js address!!!!!!!!!!
         var lossSound = new Audio('assets/sound/gameover.mp3');
         lossSound.play();
     }
-    // win:
-    if (hiddenWord === randomWord) {
+
+    // guess right:
+    for (var i = 0; i < randomWord.length; i++) {
+        if (randomWord[i] === userGuess) {
+            hiddenWord[i] = userGuess;
+            console.log(hiddenWord);
+            randomArr.length--;
+            console.log(randomArr.length);
+            guessWord = document.getElementById("guess-word");
+            guessWord.textContent = "[ " + hiddenWord + " ]";
+        }
+    }
+    if (randomArr.length === 0) {
         wins++;
-        tried = []; //reset
-        randomWord = allWords[randomNum];
+        hiddenWord = [];
+        tried = [];
+        chances = 9;
+        randomWord = allWords[Math.floor(Math.random() * allWords.length)];
+        console.log(randomWord)
+        randomArr = randomWord.split("");
+        guessWord = document.getElementById("guess-word");
+        guessWord.textContent = "[ " + hiddenWord + " ]";
+
         winPic.innerHTML = ' <img src="assets/images/cloud2.gif" class="rightpic"> ';
+        //load the audio location from the html address not js address!!!!!!!!!!
         var winSound = new Audio('assets/sound/ffvictory.mp3');
         winSound.play();
-        console.log(randomWord)
     }
 
-    gameGuide.textContent = "The Game Begin";
-    playerTyped.innerHTML = "&#9998; " + tried;
-    leftChances.textContent = "Left Chances: " + chances;
-    winTimes.textContent = "Wins: " + wins;
-    lossTimes.textContent = "Losses: " + losses;
-};
 
-guessWord.textContent = "[ " + hiddenWord + " ]";
+
+    var gameGuide = document.getElementById("guide-text");
+    gameGuide.textContent = "The Game Begin";
+    var playerTyped = document.getElementById("player-typed");
+    playerTyped.innerHTML = "&#9998; " + tried;
+    var leftChances = document.getElementById("left-chances");
+    leftChances.textContent = "Left Chances: " + chances;
+    var winTimes = document.getElementById("win-text");
+    winTimes.textContent = "Wins: " + wins;
+    var lossTimes = document.getElementById("losses-text");
+    lossTimes.textContent = "Losses: " + losses;
+}
+
